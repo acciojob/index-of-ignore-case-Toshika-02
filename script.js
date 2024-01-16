2068eb67c33972c4c024257b6619ee19b9f77856
Toshika-02 <105552050+Toshika-02@users.noreply.github.com>
2024-01-16 17:55:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
-	if(s1=== '' || s2=== ''){
-		return -1;
-	}
-	 const lower = s1.toLowerCase()
-	 const lower1 = s2.toLowerCase();
+	if (s1 === '' || s2 === '') {
+    return -1;
+  }
+ 
+  const lowerS1 = s1.toLowerCase();
+  const lowerS2 = s2.toLowerCase();
 
-	return lower.indexOf(lower1)
+  const index = lowerS1.indexOf(lowerS2);
+
+  return index;
 }
 
 Please do not change the code below
